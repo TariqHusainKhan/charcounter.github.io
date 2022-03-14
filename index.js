5db5b7b8f4c1dd28e597727a5fb9cb84dd5c300f
Tariq Husain Khan <61875008+TariqHusainKhan@users.noreply.github.com>
2022-03-14 13:17:53 +0530

    My character counter website

---
+++ index.js
@@ -0,0 +1,21 @@
+const charVal=document.getElementById("textarea");
+let totalCount=document.getElementById("total-counter");
+let remainingCount=document.getElementById("remaining-counter");
+
+let userChar=0;
+
+const updateCounter=()=>{
+    userChar=charVal.value.length;
+    totalCount.innerText=userChar;
+    remainingCount.innerText=150-userChar;
+
+};
+charVal.addEventListener("keyup",()=>updateCounter());
+
+// code of copy the text
+
+const copyText=()=>{
+    charVal.select();
+    charVal.setSelectionRange(0,9999);//mobile
+    navigator.clipboard.writeText(charVal.value);//very imp
+}
